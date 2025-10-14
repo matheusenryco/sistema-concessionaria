02385df709b46346cffd25e91526392e1acd6bea
Enryco <matheusenryco05@gmail.com>
2025-10-13 21:46:24 -0300

    refactor: clean up VeiculoService by removing whitespace

--- src/service/VeiculoService.java
+++ src/service/VeiculoService.java
@@ -8,21 +8,13 @@ import java.util.ArrayList;
 
 public class VeiculoService {
     private final List<Veiculo> veiculos;
-    
-    
+
     public VeiculoService() {
         this.veiculos = new ArrayList<>();
-        
-        
-        
     }
     
     public void cadastrar(String nome, String cor, int numMarcha, int numPorta, String marca, int ano) {
         Veiculo veiculo = new Veiculo(nome, cor, numMarcha, numPorta, marca, ano);
         veiculos.add(veiculo);
-        
     }
-    
-    
-    
 }
