8f47b92cde33efdb2b1ecb7b3e94dc8ce6e4a184
Enryco <matheusenryco05@gmail.com>
2025-10-14 13:28:56 -0300

    refactor: clean up VendasService by removing commented code and improving formatting

--- src/service/VendasService.java
+++ src/service/VendasService.java
@@ -1,7 +1,7 @@
-
+// TEM QUE PENSAR MELHOR NESSE VENDAS
 
 package service;
-/*
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import model.Vendas;
@@ -11,32 +11,21 @@ import model.Veiculo;
 
 import java.util.HashSet;
 import java.util.Set;
-*/
 
 public class VendasService {
- //   private final Set<Vendas> vendas = new HashSet();
-/*
+   private final Set<Vendas> vendas = new HashSet();
     
-    public void cadastrar(String data, double valor, Cliente cliente, Funcionario funcionario, Veiculo veiculo ){
+    public void cadastrar(String data, double valor, Cliente cliente, Funcionario funcionario, Veiculo veiculo){
         Vendas venda = new Vendas(data, valor, cliente, funcionario, veiculo);
         vendas.add(venda);
     }
     
     public Vendas consultar(Funcionario funcionario) {
-        for (Vendas venda: vendas) {
+        for (Vendas venda : vendas) {
             if (venda.getFuncionario().equals(funcionario)) {
                 return venda;
             }
         }
         return null;
-    } 
-*/ 
-
- 
-        
-        
-    
-    
-    
-    
+    }
 }
