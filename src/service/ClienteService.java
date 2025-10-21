64fbbc58ef8cad7ba8de4bdbefc2175a764dfb25
Enryco <matheusenryco05@gmail.com>
2025-10-20 21:26:25 -0300

    refactor: improve user prompts and formatting in ClienteService methods

--- src/service/ClienteService.java
+++ src/service/ClienteService.java
@@ -1,7 +1,6 @@
 package service;
 
 import model.Cliente;
-
 import java.util.Set;
 import java.util.HashSet;
 import java.util.Scanner;
@@ -12,8 +11,7 @@ public class ClienteService {
     public void cadastrar(String nome, String telefone, String email, String rg, String cpf) {
         if (buscaPorCpf(cpf) != null) {
             throw new IllegalArgumentException("Cliente com CPF " + cpf + " ja cadastrado.");
-        }
-        else {
+        } else {
             Cliente cliente = new Cliente(nome, telefone, email, rg, cpf);
             clientes.add(cliente);
         }
@@ -27,104 +25,109 @@ public class ClienteService {
         }
         return null;
     }
-    
+
     public void alterarNome() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("=== ALTERAR NOME");
+        System.out.println("=== ALTERAR NOME ===");
         System.out.print("Digite o CPF do cliente: ");
         String cpf = scanner.nextLine();
-        
+
         Cliente cliente = buscaPorCpf(cpf);
-        
-        if(cliente != null) {
+
+        if (cliente != null) {
             System.out.println("Cliente encontrado");
-            System.out.println("Digite o novo nome: ");
+            System.out.print("Digite o novo nome: ");
+
             String novoNome = scanner.nextLine();
             cliente.setNome(novoNome);
+
             System.out.println("Nome alterado com sucesso");
-            System.out.println(cliente);
-            
-            
-            
-        }   
+        } else {
+            System.out.println("Cliente nao encontrado.");
+        }
     }
-    
-    public void alterarTelefone(){ 
+
+    public void alterarTelefone() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("ALTERAR TELEFONE");
+        System.out.println("=== ALTERAR TELEFONE ===");
         System.out.print("Digite o CPF do cliente: ");
         String cpf = scanner.nextLine();
 
         Cliente cliente = buscaPorCpf(cpf);
-        
-        if(cliente != null) {
+
+        if (cliente != null) {
             System.out.println("Cliente encontrado");
-            System.out.println("Digite o novo telefone: ");
+            System.out.print("Digite o novo telefone: ");
+
             String novoTelefone = scanner.nextLine();
             cliente.setTelefone(novoTelefone);
+
             System.out.println("Telefone alterado com sucesso");
-            System.out.println(cliente);
-        
+        } else {
+            System.out.println("Cliente nao encontrado.");
+        }
     }
-    }
-    
+
     public void alterarEmail() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("ALTERAR EMAIL");
+        System.out.println("=== ALTERAR EMAIL ===");
         System.out.print("Digite o CPF do cliente: ");
         String cpf = scanner.nextLine();
 
         Cliente cliente = buscaPorCpf(cpf);
-        
-        if(cliente != null) {
+
+        if (cliente != null) {
             System.out.println("Cliente encontrado");
-            System.out.println("Digite o novo email: ");
+            System.out.print("Digite o novo email: ");
+
             String novoEmail = scanner.nextLine();
             cliente.setEmail(novoEmail);
+
             System.out.println("Email alterado com sucesso");
-            System.out.println(cliente);
-        
-        
+        } else {
+            System.out.println("Cliente nao encontrado.");
+        }
     }
-    }
-    
+
     public void alterarRg() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("ALTERAR RG");
+        System.out.println("=== ALTERAR RG ===");
         System.out.print("Digite o CPF do cliente: ");
         String cpf = scanner.nextLine();
 
         Cliente cliente = buscaPorCpf(cpf);
-        
-        if(cliente != null) {
+
+        if (cliente != null) {
             System.out.println("Cliente encontrado");
-            System.out.println("Digite o novo RG: ");
+            System.out.print("Digite o novo RG: ");
+
             String novoRg = scanner.nextLine();
             cliente.setRg(novoRg);
+
             System.out.println("RG alterado com sucesso");
-            System.out.println(cliente);
+        } else {
+            System.out.println("Cliente nao encontrado.");
         }
-        
     }
-    
+
     public void alterarCpf() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("ALTERAR CPF");
+        System.out.println("=== ALTERAR CPF ===");
         System.out.print("Digite o CPF do cliente: ");
         String cpf = scanner.nextLine();
 
         Cliente cliente = buscaPorCpf(cpf);
-        
-        if(cliente != null) {
+
+        if (cliente != null) {
             System.out.println("Cliente encontrado");
-            System.out.println("Digite o novo CPF: ");
+            System.out.print("Digite o novo CPF: ");
+
             String novoCpf = scanner.nextLine();
             cliente.setCpf(novoCpf);
+
             System.out.println("CPF alterado com sucesso");
-            System.out.println(cliente);
+        } else {
+            System.out.println("Cliente nao encontrado.");
         }
-        
     }
-    
-    
 }
