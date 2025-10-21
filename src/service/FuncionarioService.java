fd91155b3050005cb72e9be017b9cc675b62c067
Enryco <matheusenryco05@gmail.com>
2025-10-20 21:26:56 -0300

    refactor: improve formatting and user prompts in FuncionarioService methods

--- src/service/FuncionarioService.java
+++ src/service/FuncionarioService.java
@@ -1,7 +1,5 @@
-
 package service;
 
-import model.Cliente;
 import model.Funcionario;
 import java.util.HashSet;
 import java.util.Set;
@@ -13,9 +11,9 @@ public class FuncionarioService {
     public FuncionarioService() {
         this.funcionarios = new HashSet<>();
     }
-    
+
     public void cadastrar(String nome, int numMatricula, String qualificacao, String funcao, int cargaHoraria) {
-        Funcionario funcionario = new Funcionario(nome,numMatricula, qualificacao, funcao, cargaHoraria); 
+        Funcionario funcionario = new Funcionario(nome, numMatricula, qualificacao, funcao, cargaHoraria);
         funcionarios.add(funcionario);
     }
 
@@ -27,96 +25,114 @@ public class FuncionarioService {
         }
         return null;
     }
-    
+
     public void alterarNome() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("--- ALTERAR NOME ---");
+        System.out.println("=== ALTERAR NOME ===");
         System.out.print("Digite o numero de matricula do funcionario: ");
         int numMatricula = scanner.nextInt();
         scanner.nextLine();
-        
+
         Funcionario funcionario = buscaPorMatricula(numMatricula);
-        
-        if(funcionario != null) {
+
+        if (funcionario != null) {
             System.out.println("Funcionario encontrado");
-            System.out.println("Digite o novo nome: ");
+            System.out.print("Digite o novo nome: ");
+
             String novoNome = scanner.nextLine();
             funcionario.setNome(novoNome);
+
             System.out.println("Nome alterado com sucesso");
-            System.out.println(funcionario);
-            
+        } else {
+            System.out.println("Funcionario nao encontrado.");
+        }
     }
-    }
-    
+
     public void alterarNumMatricula() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("--- ALTERAR MATRICULA ---");
+        System.out.println("=== ALTERAR MATRICULA ===");
         System.out.print("Digite o numero de matricula do funcionario: ");
         int numMatricula = scanner.nextInt();
         scanner.nextLine();
-        
+
         Funcionario funcionario = buscaPorMatricula(numMatricula);
-        
-        if(funcionario != null) {
+
+        if (funcionario != null) {
             System.out.println("Funcionario encontrado");
-            System.out.println("Digite o novo numero de matricula: ");
+            System.out.print("Digite o novo numero de matricula: ");
+
             int novoNumMatricula = scanner.nextInt();
             funcionario.setNumMatricula(novoNumMatricula);
+
             System.out.println("Matricula alterada com sucesso");
-            System.out.println(funcionario);}
+        } else {
+            System.out.println("Funcionario nao encontrado.");
+        }
     }
+
     public void alterarQualificacao() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("--- ALTERAR QUALIFICACAO ---");
+        System.out.println("=== ALTERAR QUALIFICACAO ===");
         System.out.print("Digite o numero de matricula do funcionario: ");
         int numMatricula = scanner.nextInt();
         scanner.nextLine();
-        
+
         Funcionario funcionario = buscaPorMatricula(numMatricula);
-        
-        if(funcionario != null) {
+
+        if (funcionario != null) {
             System.out.println("Funcionario encontrado");
-            System.out.println("Digite a nova qualificacao: ");
-            String novoQualificacao = scanner.nextLine();
-            funcionario.setQualificacao(novoQualificacao);
+            System.out.print("Digite a nova qualificacao: ");
+
+            String novaQualificacao = scanner.nextLine();
+            funcionario.setQualificacao(novaQualificacao);
+
             System.out.println("Qualificacao alterada com sucesso");
-            System.out.println(funcionario);}
+        } else {
+            System.out.println("Funcionario nao encontrado.");
+        }
     }
-    
+
     public void alterarDescricaoFuncao() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("--- ALTERAR DESCRICAO DE FUNCAO ---");
+        System.out.println("=== ALTERAR DESCRICAO DE FUNCAO ===");
         System.out.print("Digite o numero de matricula do funcionario: ");
         int numMatricula = scanner.nextInt();
         scanner.nextLine();
-        
+
         Funcionario funcionario = buscaPorMatricula(numMatricula);
-        
-        if(funcionario != null) {
+
+        if (funcionario != null) {
             System.out.println("Funcionario encontrado");
-            System.out.println("Digite a nova descricao de funcao: ");
-            String novoDescricao = scanner.nextLine();
-            funcionario.setDescricaoFuncao(novoDescricao);
+            System.out.print("Digite a nova descricao de funcao: ");
+
+            String novaDescricao = scanner.nextLine();
+            funcionario.setDescricaoFuncao(novaDescricao);
+
             System.out.println("Descricao alterada com sucesso");
-            System.out.println(funcionario);}
-        
+        } else {
+            System.out.println("Funcionario nao encontrado.");
+        }
     }
-    
+
     public void alterarCargaHoraria() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("--- ALTERAR CARGA HORARIA SEMANAL ---");
+        System.out.println("=== ALTERAR CARGA HORARIA SEMANAL ===");
         System.out.print("Digite o numero de matricula do funcionario: ");
         int numMatricula = scanner.nextInt();
         scanner.nextLine();
-        
+
         Funcionario funcionario = buscaPorMatricula(numMatricula);
-        
-        if(funcionario != null) {
+
+        if (funcionario != null) {
             System.out.println("Funcionario encontrado");
-            System.out.println("Digite a nova carga horaria");
-            int novoCargaHoraria = scanner.nextInt();
-            funcionario.setCargaHorariaSemanal(novoCargaHoraria);
+            System.out.print("Digite a nova carga horaria: ");
+
+            int novaCargaHoraria = scanner.nextInt();
+            funcionario.setCargaHorariaSemanal(novaCargaHoraria);
+            
             System.out.println("Carga horaria alterada com sucesso");
-            System.out.println(funcionario);}
+        } else {
+            System.out.println("Funcionario nao encontrado.");
+        }
     }
 }
